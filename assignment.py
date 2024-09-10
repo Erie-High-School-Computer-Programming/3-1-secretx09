a059373372099eb24b5f26336b731788050cf540
secretx09 <101412985+secretx09@users.noreply.github.com>
2024-09-10 14:05:55 +0000

    im done

--- assignment.py
+++ assignment.py
@@ -5,52 +5,59 @@ def return_first_element(lst: list) -> str:
     Returns the first element of the input list
     Remember that the first element is at index 0
     """
-    raise NotImplementedError()
+    return lst[0]
 
 
 def return_last_element(lst: list) -> str:
     """
     Returns the last element of the input list
     """
-    raise NotImplementedError()
+    return lst[-1]
 
-def add_element_to_an_fsrting(lst: list) -> str:
+def add_element_to_a_string(lst: list) -> str:
     """
     Access the first element of the list and return it as in the sentence
     Modify the element so it is in title case.
     "The first element of the list is: <element>"
     """
-    raise NotImplementedError()
+    element = lst[0].title()
+    return f"The first element of the list is: {element}"
 
 def change_the_first_element(lst: list) -> list:
     """
     Change the first element of the list to "Python"
     """
-    raise NotImplementedError() 
+    lst[0] = "Python"
+    return lst
 
 def change_the_last_element(lst: list) -> list:
     """
     Change the last element of the list to "Spam"
     """
-    raise NotImplementedError()
+    lst[-1] = "Spam"
+    return lst
 
 def add_element_to_the_end(lst: list) -> list:
     """
     Add the element "Eggs" to the end of the list
     """
-    raise NotImplementedError()
+    lst.append("Eggs")
+    return lst
 
 def add_element_to_the_beginning(lst: list) -> list:
     """
     Add the element "Eggs" to the beginning of the list
     """
-    raise NotImplementedError()
+    lst.insert(0, 0)
+    lst.insert(0, "Eggs")
+    return lst
 
 def remove_last_element(lst: list) -> list:
     """
     Remove the last element of the list
     """
-    raise NotImplementedError()
+    lst.pop()
+    return lst
 
 def remove_first_element(lst: list) -> tuple[list, str]:    
     """
@@ -59,35 +66,41 @@ def remove_first_element(lst: list) -> tuple[list, str]:
     Be sure to return the list and the removed element separated by a comma
     Ex: return lst, removed_element
     """
-    raise NotImplementedError()
+    removed_element = lst[0]
+    lst.pop(0)
+    return lst, removed_element
 
 def remove_element_at_index(lst: list, index: int) -> list:
     """
     Remove the element at the index provided as an argument
     """
-    raise NotImplementedError()
+    lst.pop(index)
+    return lst
 
 def insert_element_at_index(lst: list, index: int, element: str) -> list:
     """
     Insert the element at the index provided as an argument
     """
-    raise NotImplementedError()
+    lst.insert(2, element)
+    return lst
 
 def remove_element_by_value(lst: list, element: str) -> list:
     """
     Remove the element by value
     """
-    raise NotImplementedError()
+    lst.pop(2)
+    return lst
 
 def sort_list_alphabetically(lst: list) -> list:
     """
     Sort the list alphabetically
     """
-    raise NotImplementedError()
+    lst.sort()
+    return lst
 
 def reverse_list(lst: list) -> list:
     """
     Reverse the list
     """
-    raise NotImplementedError()
-
+    lst = lst[::-1]
+    return lst
